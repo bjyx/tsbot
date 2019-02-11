99b5ce32cab3e7a7f0054b6445fe02cb505c84db
[REDACTED] [REDACTED] <wes4zhang@gmail.com>
2019-02-10 23:58:38 -0800

    Stuff

--- TSBot/src/main/Listener.java
+++ TSBot/src/main/Listener.java
@@ -14,7 +14,7 @@ public class Listener extends ListenerAdapter {
         String content = message.getContentRaw(); 
         // getContentRaw() is an atomic getter
         // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
-        if (content.equals("!ping"))
+        if (content.charAt(0)=='!')
         {
             MessageChannel channel = event.getChannel();
             channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
