72001ef74d1c8e3a63d63097750050bb4380147e
[REDACTED] [REDACTED] <wes4zhang@gmail.com>
2019-02-12 12:38:04 -0800

    Added LeaveCommand

--- TSBot/src/commands/LeaveCommand.java
+++ TSBot/src/commands/LeaveCommand.java
@@ -6,42 +6,38 @@ import game.PlayerList;
 
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
-public class JoinCommand extends Command {
+public class LeaveCommand extends Command {
 	
 	private PlayerList plist = new PlayerList();
 	@Override
 	public void onCommand(MessageReceivedEvent e, String[] args) {
 		//TODO: if game started return;
-		if (Arrays.asList(plist.getArray()).contains(e.getAuthor())) {
-			sendMessage(e, ":x: You're already on the list.");
+		//TODO: if game over return;
+		if (!Arrays.asList(plist.getArray()).contains(e.getAuthor())) {
+			sendMessage(e, ":x: You're not on the list.");
 			return;
 		}
-		if (Arrays.asList(plist.getArray()).contains(null)) {
-			plist.addPlayer(e.getAuthor());
-			sendMessage(e, ":o: Done");
-			return;
-		}
-		sendMessage(e, ":x: There is only enough room for two superpowers on this world stage.");
+		plist.removePlayer(e.getAuthor());
 	}
 
 	@Override
 	public List<String> getAliases() {
-		return Arrays.asList("TS.join");
+		return Arrays.asList("TS.leave");
 	}
 
 	@Override
 	public String getDescription() {
-		return "Join a game that hasn't started. ";
+		return "Leave a game that hasn't started. ";
 	}
 
 	@Override
 	public String getName() {
-		return "Join";
+		return "Leave";
 	}
 
 	@Override
 	public List<String> getUsageInstructions() {
-		return Arrays.asList("TS.join - Join a game.");
+		return Arrays.asList("TS.leave - Leave a game.");
 	}
 
 }
