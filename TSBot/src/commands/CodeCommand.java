8d2c2d0b5174b8ae7879f23d28a61afcf6bd9cb1
[REDACTED] [REDACTED] <wes4zhang@gmail.com>
2019-02-12 11:51:14 -0800

    TS.code and TS.join

---
+++ TSBot/src/commands/CodeCommand.java
@@ -0,0 +1,37 @@
+package commands;
+
+import java.util.Arrays;
+import java.util.List;
+
+import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class CodeCommand extends Command {
+
+	@Override
+	public void onCommand(MessageReceivedEvent e, String[] args) {
+		MessageBuilder message = new MessageBuilder().append("https://github.com/bjyx/tsbot");
+		sendMessage(e, message.build());
+	}
+
+	@Override
+	public List<String> getAliases() {
+		return Arrays.asList("TS.code");
+	}
+
+	@Override
+	public String getDescription() {
+		return "In case you wanted to learn about the bot's inner workings.";
+	}
+
+	@Override
+	public String getName() {
+		return "Code Dump";
+	}
+
+	@Override
+	public List<String> getUsageInstructions() {
+		return Arrays.asList("TS.code - Returns a link that can show you the inner workings of the bot.");
+	}
+
+}
---
+++ TSBot/src/game/PlayerList.java
@@ -0,0 +1,66 @@
+package game;
+
+import java.util.Random;
+
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.User;
+
+public class PlayerList {
+	private User[] players;
+	
+	public PlayerList() {
+		players = new User[2];
+		players[0]=null;
+		players[1]=null;
+	}
+	
+	public void addPlayer(User u) {
+		if (players[0]==null) {
+			players[0]=u;
+			return;
+		}
+		if (players[1]==null) {
+			players[1]=u;
+			return;
+		}
+	}
+	public void removePlayer(User u) {
+		if (players[0]==u) {
+			players[0]=null;
+			return;
+		}
+		if (players[1]==u) {
+			players[1]=null;
+			return;
+		}
+	}
+	public void detSides() {
+		Random rng = new Random();
+		int side = rng.nextInt(1);
+		if(side == 0) return;
+		User temp = players[0];
+		players[0] = players[1];
+		players[1] = temp;
+	}
+	public MessageEmbed getPlayers(boolean started) {
+		EmbedBuilder builder = new EmbedBuilder().setTitle("Superpowers").setColor(8519882);
+		if (started) {
+			builder.setDescription(":flag_us: " + players[0] + "\n:flag_su: " + players[1]);
+		}
+		else {
+			builder.setDescription(players[0] + "\n" + players[1]);
+		}
+		return builder.build();
+	}
+	public User getUSA() {
+		return players[0];
+	}
+	public User getSSR() {
+		return players[1];
+	}
+	public User getPhasing(int ar) {
+		if (ar==0) return null; //(I can't deal with this yet)
+		return players[ar%2]; //ar1 is USSR, ar2 is US, and so forth until ar12/14/16
+	}
+}
