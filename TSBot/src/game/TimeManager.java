ec43789901e4d9ce16f460321a8d219827fa163c
[REDACTED] [REDACTED] <wes4zhang@gmail.com>
2019-02-25 12:54:36 -0800

    Create TimeManager.java

---
+++ TSBot/src/game/TimeManager.java
@@ -0,0 +1,17 @@
+package game;
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class TimeManager extends ListenerAdapter {
+	@Override
+    public void onMessageReceived(MessageReceivedEvent e)
+    {
+        if (!e.getAuthor().getId().equals("519675083644469279"))
+            return;
+        if (e.getMessage().getContentRaw().startsWith("`ADVANCE`")) {
+        	GameData.advanceTime();
+        }
+            
+    }
+}
