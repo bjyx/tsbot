716d7c9250725eb734fde51e4f427ebf5edcbb56
[REDACTED] [REDACTED] <wes4zhang@gmail.com>
2020-03-05 18:35:50 -0800

    Added Reset button

---
+++ TSBot/src/commands/ResetCommand.java
@@ -0,0 +1,50 @@
+package commands;
+
+import java.util.Arrays;
+import java.util.List;
+
+import game.GameData;
+import game.PlayerList;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class ResetCommand extends Command {
+	
+	private static boolean[] consent = {false, false};
+
+	@Override
+	public void onCommand(MessageReceivedEvent e, String[] args) {
+		consent[PlayerList.getArray().indexOf(e.getAuthor())]=true;
+		if (!GameData.hasGameEnded()&&!(consent[0]&&consent[1])) {
+			sendMessage(e, ":warning: You sure about this? Get the other person to agree.");
+			
+			return;
+		}
+		consent[0]=false;
+		consent[1]=false;
+		GameData.reset();
+		
+	}
+
+	@Override
+	public List<String> getAliases() {
+		// TODO Auto-generated method stub
+		return Arrays.asList("TS.reset");
+	}
+
+	@Override
+	public String getDescription() {
+		return "Get ready for a do-over. **Both players need to agree if the game isn't over yet.**";
+	}
+
+	@Override
+	public String getName() {
+		return "Reset";
+	}
+
+	@Override
+	public List<String> getUsageInstructions() {
+		// TODO Auto-generated method stub
+		return Arrays.asList("TS.reset - Resets all variables. Get ready to do this again.");
+	}
+
+}
